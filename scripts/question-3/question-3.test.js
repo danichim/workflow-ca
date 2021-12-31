e7022855282b0f94be75a2b4133787077bf0fb48
Dan Ichim <danichim.work@gmail.com>
2021-12-31 12:28:51 +0200

    Update question-3.test.js

--- scripts/question-3/question-3.test.js
+++ scripts/question-3/question-3.test.js
@@ -1,8 +1,11 @@
 import { expect } from "@jest/globals";
-import { getDiscountPercentage } from "./question-3";
+import { getDiscountPercentage, formatDecimalSpaces } from "./question-3";
 
-describe("get discount percentage", () => {
+describe("test functions question 3", () => {
     test("it should get percentage", () => {
         expect(getDiscountPercentage(100, 75)).toBe(25);
-   })   
+    })
+    test("it should Converts a value to 2 decimal spaces", () => {
+        expect(formatDecimalSpaces(15.5555555)).toBe(15.56);
+    })
 });
